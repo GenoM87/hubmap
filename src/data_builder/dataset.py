dc17c89d52460de6e1b97cf151fbaeb28427beea
Giorgio <mombelligiorgio@gmail.com>
2020-12-29 14:49:34 +0100

    avvio check image

--- src/data_builder/dataset.py
+++ src/data_builder/dataset.py
@@ -10,7 +10,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
 
-class hmDataset(Dataset):
+class hmTrainDataset(Dataset):
 
     def __init__(self, df, img_ids, cfg, transforms=None, preprocessing=None):
         self.img_ids = img_ids
@@ -37,7 +37,7 @@ class hmDataset(Dataset):
             os.path.join(path_img, 'x'+str(row['cx'])+'_y'+str(row['cy'])+'.mask.png'),
             cv2.IMREAD_GRAYSCALE
         )
-
+        
         if self.transforms:
             augmented = self.transforms(image=img, mask=mask)
             img = augmented['image']
@@ -46,4 +46,37 @@ class hmDataset(Dataset):
             preprocessed = self.preprocessing(image=img, mask=mask)
             img = preprocessed['image']
             mask = preprocessed['mask']
+
         return img, mask
+
+class hmTestDataset(Dataset):
+
+    def __init__(self, df, img_ids, cfg, transforms=None, preprocessing=None):
+        self.img_ids = img_ids
+        self.df = df[df['image_id'].isin(img_ids)]
+        self.cfg = cfg
+        self.tile_dir = cfg.DATASET.TILE_DIR
+        self.transforms = transforms
+        self.preprocessing = preprocessing
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx:int):
+        row = self.df.iloc[idx]
+        path_img = os.path.join(
+            self.tile_dir, 
+            row['image_id']
+        )
+        img = cv2.imread(
+            os.path.join(path_img, 'x'+str(row['cx'])+'_y'+str(row['cy'])+'.png'),
+            cv2.IMREAD_COLOR
+        )
+
+        if self.transforms:
+            augmented = self.transforms(image=img)
+            img = augmented['image']
+        if self.preprocessing:
+            preprocessed = self.preprocessing(image=img)
+            img = preprocessed['image']
+        return img
